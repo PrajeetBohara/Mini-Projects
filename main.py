61a472d1f71d41ca7d47c9a2d7bb77f325ab195b
Prajeet Bohara <153901646+PrajeetBohara@users.noreply.github.com>
2025-02-24 23:05:18 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+import smtplib
+import datetime as dt
+
+my_email = "halloapp77@gmail.com"
+password = "mydduamddtteyhby"
+receiver = "rpgjohnraven@gmail.com"
+with open("quotes.txt", encoding="utf8") as file:
+    content = file.readlines()
+    quote = random.choice(content)
+    print(quote)
+my_date = dt.datetime(year=2024,month=12,day=28,hour=7,minute=43,second=00)
+current_date = dt.datetime.now()
+print(f"My date: {my_date}")
+print(f"Current date: {current_date}")
+
+while my_date == current_date:
+    connection = smtplib.SMTP("smtp.gmail.com", port=587)
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs=receiver, msg="Subject: Demo\n\n Check")
+    connection.close()
+
+
